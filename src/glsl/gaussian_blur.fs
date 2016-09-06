7bcc027344df18b2a481e58064dbe1a93572b02f
yoichi kobayashi <info@tplh.net>
2016-09-06 14:02:31 +0900

    [add] npm dependencies 'glsl-fast-gaussian-blur'.

--- src/glsl/gaussian_blur.fs
+++ src/glsl/gaussian_blur.fs
@@ -4,9 +4,11 @@ uniform sampler2D texture;
 
 varying vec2 vUv;
 
-#pragma glslify: blur = require('./modules/blur13.glsl');
+#pragma glslify: blur13 = require('glsl-fast-gaussian-blur/13')
+#pragma glslify: blur9 = require('glsl-fast-gaussian-blur/9')
+#pragma glslify: blur5 = require('glsl-fast-gaussian-blur/5')
 
 void main(void) {
   vec4 colorBase = texture2D(texture, vUv);
-  gl_FragColor = blur(texture, vUv, resolution, direction);
+  gl_FragColor = blur13(texture, vUv, resolution, direction);
 }
