785a0226d4ebaa1fb996597d8a91842b62c5262c
yoichi kobayashi <info@tplh.net>
2016-09-12 23:46:46 +0900

    [add] click event for moving light.

--- src/glsl/sphere.fs
+++ src/glsl/sphere.fs
@@ -1,10 +1,12 @@
+uniform vec2 cursor;
+
 varying vec4 vColor;
 varying vec4 vPosition;
 varying mat4 vInvertMatrix;
 
 void main(void) {
   vec3 normal = normalize(cross(dFdx(vPosition.xyz), dFdy(vPosition.xyz)));
-  vec3 inv_light = normalize(vInvertMatrix * vec4(vec3(0.0, 1.0, 0.0), 0.0)).xyz;
+  vec3 inv_light = normalize(vInvertMatrix * vec4(vec3(cursor.x, cursor.y, 1.0 - abs(cursor.x)), 0.0)).xyz;
   float diff = (dot(normal, inv_light) + 1.0) / 2.0;
   vec3 light = vec3(0.4) * diff;
   vec4 color = vColor;
--- src/js/modules/sphere.js
+++ src/js/modules/sphere.js
@@ -6,6 +6,7 @@ export default class Sphere {
     this.hue = 0.1;
     this.uniforms = null;
     this.mesh = this.createMesh();
+    this.setEvent();
   }
   createMesh() {
     this.uniforms = {
@@ -16,6 +17,10 @@ export default class Sphere {
       hue: {
         type: 'f',
         value: this.hue,
+      },
+      cursor: {
+        type: 'v2',
+        value: new THREE.Vector2(-0.5, 1),
       },
     };
     return new THREE.Mesh(
@@ -29,6 +34,14 @@ export default class Sphere {
         shading: THREE.FlatShading,
       })
     );
+  }
+  setEvent() {
+    document.getElementById('canvas-webgl').addEventListener('click', (event) => {
+      this.uniforms.cursor.value.set(
+        event.clientX / window.innerWidth * 2 - 1,
+        (window.innerHeight - event.clientY) / window.innerHeight * 2 - 1
+      );
+    });
   }
   render(time) {
     this.uniforms.time.value += time * this.time;
